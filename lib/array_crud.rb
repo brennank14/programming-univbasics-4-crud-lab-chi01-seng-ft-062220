fb5ea36c1dcf0413b337e5952e352956addf2676
Kathleen Brennan <brennank14@gmail.com>
2020-06-05 23:14:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,19 +7,20 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["hello", "hi"]
+  array = ["wow", "I", "am", "really", "learning"]
   element = "arrays!"
   array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["hi", "hello"]
+  array = ["I", "am", "really", "learning"]
   element = "wow"
   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
