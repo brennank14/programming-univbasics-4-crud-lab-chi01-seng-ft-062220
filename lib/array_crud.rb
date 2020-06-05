0bf3eb05a1ac8456ea1ce29d5b887d6d8048a301
Kathleen Brennan <brennank14@gmail.com>
2020-06-05 23:17:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,12 +29,12 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["I", "am", "really", "learning", "arrays!"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array[2]
 end
 
 def retrieve_first_element_from_array(array)
-array = ["I", "am", "really", "learning", "arrays!"]
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array[0]
 end
 
@@ -45,5 +45,5 @@ end
 
 def update_element_from_index(array, index_number, element)
   array = ["I", "am", "really", "learning", "arrays!"]
-  array[4] = 'totaly'
+  array[4] = 'totally'
 end
